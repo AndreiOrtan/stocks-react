b61206c4e1147eccd49904a11915cfba882f05fb
ortanandrei <ortanandrei2000@gmail.com>
2022-12-20 20:08:24 +0200

    modal clean code

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useRef } from "react";
+import { ReactNode } from "react";
 import "./Modal.css";
 import ReactDom from "react-dom";
 
@@ -9,28 +9,12 @@ interface Modals {
 }
 
 const Modal = ({ children, isOpen, setIsOpen }: Modals) => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const eventHandler = (e: any) => {
-      if (ref.current?.contains(e.target) || e.target.textContent === "Edit") {
-        return;
-      }
-      setIsOpen(false);
-    };
-    document.body.addEventListener("click", eventHandler);
-    return () => {
-      document.body.removeEventListener("click", eventHandler);
-    };
-  }, []);
-
   if (!isOpen) return null;
 
   return ReactDom.createPortal(
     <>
-      <div className="overlay"></div>
-      <div className="modal_styles" ref={ref}>
-        {children}
+      <div className="overlay" onClick={() => setIsOpen(false)}>
+        <div className="modal_styles">{children}</div>
       </div>
     </>,
     document.getElementById("portal") as HTMLDivElement
